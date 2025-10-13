9c6acb737ccfa39ef207daadc189ab8a2e437661
Paulo Fernando <61089612+Paulo-Fernando-R@users.noreply.github.com>
2025-10-13 14:15:41 -0300

    `feat: Update HomeController and HomeMobile to use react-router's useSearchParams hook`

--- frontend/src/pages/home/HomeMobile.tsx
+++ frontend/src/pages/home/HomeMobile.tsx
@@ -7,14 +7,32 @@ import HomeController from "./homeController";
 import type { DropdowItem } from "../../components/dropdown/Dropdown";
 import { useEffect, useRef, useState } from "react";
 import Feed, { FeedError, FeedPlaceholder } from "../../components/Feed/Feed";
+import { useSearchParams } from "react-router";
 
 export default function HomeMobile() {
-    const [city, setCity] = useState<DropdowItem | null>(null);
-    const [type, setType] = useState<DropdowItem | null>(null);
-    const [search, setSearch] = useState("");
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const [city, setCity] = useState<DropdowItem | null>(
+        HomeController.getCityFromUrl(searchParams)
+    );
+    const [type, setType] = useState<DropdowItem | null>(
+        HomeController.getTypeFromUrl(searchParams)
+    );
+    const [search, setSearch] = useState(HomeController.getSearchFromUrl(searchParams));
+
     const [showImg, setShowImg] = useState(true);
     const firstRender = useRef(true);
-    const controller = new HomeController(setCity);
+
+    const controller = new HomeController(
+        city,
+        setCity,
+        type,
+        setType,
+        search,
+        setSearch,
+        searchParams,
+        setSearchParams
+    );
 
     const { data, isLoading } = useQuery({
         queryKey: ["cities"],
@@ -33,6 +51,7 @@ export default function HomeMobile() {
 
     function refetch() {
         infiniteQuery.refetch();
+        controller.addParams();
         setShowImg(false);
     }
     function fetchNextPage() {
