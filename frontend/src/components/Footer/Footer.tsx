a2a7b76f30ea21202eea4f44fe69641a52800781
Paulo Fernando <61089612+Paulo-Fernando-R@users.noreply.github.com>
2025-11-13 16:18:47 -0300

    `Refactor Footer component: update styles and layout`

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -3,21 +3,25 @@ import logo from "../../assets/images/logo_alt1.png";
 export default function Footer() {
     return (
         <div className={styles.footer}>
-            <div className={styles.left}>
-                <img src={logo} alt="Logo" />
-                <h2 className={styles.title + " titleh3"}>Saúde Localiza</h2>
-            </div>
+            <div className={styles.section1}>
+                <div className={styles.left}>
+                    <img src={logo} alt="Logo" />
+                    <h2 className={styles.title + " p1"}>Saúde Localiza</h2>
+                </div>
 
-            <div className={styles.developers}>
-                <h4 className="p1">Desenvolvido por:</h4>
-                <a href="https://www.linkedin.com/in/paulo-fernando-071bb31a9/">Paulo Rodrigues</a>
-                <a href="https://www.linkedin.com/in/theu-ferreira/">Matheus Ferreira</a>
+                <div className={styles.developers}>
+                    <h4 className="p2">Desenvolvido por:</h4>
+                    <a href="https://www.linkedin.com/in/paulo-fernando-071bb31a9/">
+                        Paulo Rodrigues
+                    </a>
+                    <a href="https://www.linkedin.com/in/theu-ferreira/">Matheus Ferreira</a>
+                </div>
             </div>
 
             <p>
                 O objetivo deste site é ajudar as pessoas a encontrar facilmente hospitais,
                 clínicas, laboratórios e postos de saúde em qualquer lugar do Brasil.
-                <br /> <br /> O site utiliza dados públicos oficiais do CNES (Cadastro Nacional de
+                <br /> O site utiliza dados públicos oficiais do CNES (Cadastro Nacional de
                 Estabelecimentos de Saúde), do Ministério da Saúde.
             </p>
         </div>
